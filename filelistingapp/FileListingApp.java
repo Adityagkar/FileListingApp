45d86fbd5ad5e034c705905e793bdcdc74ed02ec
ghodgaonkaraditya <ghodgaonkaraditya@gmail.com>
2018-02-06 13:24:12 +0530

    Code Improvised - Modularity Increased !

--- filelistingapp/FileListingApp.java
+++ filelistingapp/FileListingApp.java
@@ -19,7 +19,7 @@ public class FileListingApp {
      static List l= new ArrayList();
             
      
-    public static void main(String[] args) throws IOException,NullPointerException {
+    public static void main(String[] args) throws IOException {
       
         /*
         Scanner scan=new Scanner(System.in);
@@ -36,12 +36,11 @@ public class FileListingApp {
         String input_line="Contains source path";
         String output_line="Contains destination path";
         FileReader fr=null;
-        BufferedReader br=null;
         
         //for getting the source and destination path from given text file (whose path is supplied by user)
-            try{
-                fr = new FileReader(user_input);
-                br = new BufferedReader(fr);
+            try {
+                fr = new FileReader(user_input); 
+                BufferedReader br = new BufferedReader(fr);
                 input_line = br.readLine(); //reads the first line which is Target path(for input)
                 output_line = br.readLine(); // reads the second line which is Destination path (for output)
 
@@ -50,47 +49,30 @@ public class FileListingApp {
                 if(fr!=null)
                     fr.close();           
             }
-                 
+            
+            BufferedWriter out=null;
+            
+            try{
+
              File input = new File(input_line);
              FileWriter output = new FileWriter(output_line);
-<<<<<<< HEAD
-             BufferedWriter out=new BufferedWriter(output);
-
-                Lister(input);//Call to Lister method
-  
-                Writer(out); //Call to Writer function to Transfer ArrayList to CSV
-                String display=(size/2)+" were written successfully at "+output_line;
-                JOptionPane.showMessageDialog(null,display);
-        
-=======
              out=new BufferedWriter(output);
 
-             Lister(input);
-            // System.out.println(size+" Files were stored at "+output_line);
-             
-         //Code to Transfer ArrayList to CSV - Path for csv will be stored in output_line
+             Lister(input);// call to Lister
+             Writer(out);// call to Writer
          
                     String display=(size/2)+" were written successfully at "+output_line;
                     JOptionPane.showMessageDialog(null,display);
 
-                    for(int i=0;i<size;i++){
-                          System.out.println(l.get(i));
-                        out.write(l.get(i).toString()); 
-                        out.write(",");
-                        if((i+1)%2==0)
-                            out.write("\n");
-                        out.flush();
-                    }
-                    }
+                    
+             }
              catch(NullPointerException e)
              {
                  System.out.print("NullPointerException Caught"+e);
              } 
       
->>>>>>> 77750cba157b371020d32f095a3f2d918949d575
       //end of main()  
     }
-    
     
     // Recursive function Lister
     /*
@@ -101,45 +83,47 @@ public class FileListingApp {
          
           File lists[]=input.listFiles();
    
-            //if input points to a directory then generate all the files and folders list into an array called lists[]   
-            if(input.isDirectory()){ 
+       //if input points to a directory then generate all the files and folders list into an array called lists[]   
+       if(input.isDirectory()){ 
+           
+            
+        // say the array is lists
+           
+                //for each file/folder stored in array lists[]
+                for(int i=0;i<lists.length;i++){
+
+                        //check if it's a File or Directory
+                        if(lists[i].isFile()){  // Also, a break condition for Recursion.
+                            //if it's a file write it's path to an ArrayList
+                            l.add(lists[i].getName());
+                            size++;
+                            l.add(lists[i].getAbsoluteFile());
+                            //System.out.println("File name: "+lists[i].getName()+" and Path :"+lists[i].getPath());
+                            size++;
 
 
-                   // say the array is lists
-                   //for each file/folder stored in array lists[]
-                   for (File list : lists) {
-                       //check if it's a File or Directory
-                       if (list.isFile()) {
-                           // Also, a break condition for Recursion.
-                           //if it's a file write it's path to an ArrayList
-                           l.add(list.getName());
-                           size++;
-                           l.add(list.getAbsoluteFile());
-                           //System.out.println("File name: "+lists[i].getName()+" and Path :"+lists[i].getPath());
-                           size++;
-                       } else {
-                           // if it's not a file then initialise File handler "input" with it's path
-                           //Recall the function Lister() by passing the updated File Handler
-                           File temp = new File(list.getPath());
-                           Lister(temp); //Recursive call
-                       }
-                   }
+                        }
+                        else{
+                            // if it's not a file then initialise File handler "input" with it's path  
+                            //Recall the function Lister() by passing the updated File Handler
+                            File temp=new File(lists[i].getPath());
+                            Lister(temp); //Recursive call
+                        }
 
-             }
-            else{
-                //if input points to a file add it to list and terminate Lister ()
-                 l.add(lists[0].getAbsoluteFile());
-            }
-
-<<<<<<< HEAD
-
+                    }
+        }
+       else{
+           //if input points to a file add it to list and terminate Lister ()
+            l.add(lists[0].getAbsoluteFile());
+       }
+         
     }// end of function Lister()
-    
-     /*
+      
+    /*
     Writer() will take a single argument as a File handler which will be the destination path where we have
     to write the ArrayList 
-     */          
-    static void Writer(BufferedWriter out) throws IOException{
+     */   
+        static void Writer(BufferedWriter out) throws IOException{
          
                     for(int i=0;i<size;i++){
                           System.out.println(l.get(i));
@@ -150,24 +134,7 @@ public class FileListingApp {
                         
                         out.flush();
                     }
-    }//end of CSV Writer function
-   
-=======
-                    }
-            
-        }
-       else{
-           //if input points to a file add it to list and terminate Lister ()
-            l.add(lists[0].getAbsoluteFile());
-       }
-           
-            //This is for testing whether the file paths are been written in ArrayList or not
-            // for(int i=0;i<size;i++)
-            //    System.out.println(l.get(i));
+        }//end of CSV Writer function
     
-         
-    }// end of function Lister()
- 
->>>>>>> 77750cba157b371020d32f095a3f2d918949d575
  // end of class   
 }
